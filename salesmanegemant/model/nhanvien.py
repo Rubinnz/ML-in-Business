576f513909267ee62d69f21c94bdabdc5ff5f8d4
Rubindz <vuquockhanhdz@gmail.com>
2025-01-23 16:38:13 +0700

    Load Full

---
+++ salesmanegemant/model/nhanvien.py
@@ -0,0 +1,9 @@
+class Nhanvien:
+    def __init__(self,ID=None, manhanvien = None, tennhanvien=None,
+                 username = None,password=None, isdelete = None ):
+        self.ID = ID
+        self.manhanvien = manhanvien
+        self.tennhanvien = tennhanvien
+        self.username = username
+        self.password = password
+        self.isdelete = isdelete
